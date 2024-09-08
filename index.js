4b1ca6b4a1d27ff8384c881c6fff72f4eb4ad680
Abhishek Sahu <abhisheksahu504@gmail.com>
2024-09-08 12:28:20 +0530

    nan value also comes under number

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+"use strict";
 console.log("xyz");
-var num = 5;
+let num = 5;
+//* func define
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(7, 8));
+let nanvalue = NaN;
--- index.ts
+++ index.ts
@@ -1,2 +1,10 @@
 console.log("xyz");
 let num: number = 5;
+
+//* func define
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(sum(7, 8));
+let nanvalue: number = NaN;
