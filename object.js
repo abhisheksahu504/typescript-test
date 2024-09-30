adbfe92ca8f304cc53a58d2a960c8f81f088c847
Abhishek Sahu <abhisheksahu504@gmail.com>
2024-09-30 11:37:33 +0530

    defining objects

---
+++ object.js
@@ -0,0 +1,18 @@
+"use strict";
+// defining objects in typescript
+const student = {
+    name: "ABCD",
+    id: 45,
+    isStudent: true,
+    address: {
+        city: "Mumbai",
+        country: "India",
+    },
+};
+//another object
+const product = {
+    name: "Laptop",
+    price: 45000,
+    quantity: 5,
+};
+console.log(product);
---
+++ object.ts
@@ -0,0 +1,21 @@
+// defining objects in typescript
+const student: {
+  name: string;
+  id: number;
+  isStudent: boolean;
+  address: { city: string; country: string };
+} = {
+  name: "ABCD",
+  id: 45,
+  isStudent: true,
+  address: {
+    city: "Mumbai",
+    country: "India",
+  },
+};
+//another object
+const product: { name: string; price: number; quantity: number } = {
+  name: "Laptop",
+  price: 45000,
+  quantity: 5,
+};
